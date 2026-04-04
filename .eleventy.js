0604f5ae60be24212fb9bdbc0efe531d47c5ff93
Tom Williams <108416997+TomWilliamsBrown@users.noreply.github.com>
2026-04-03 21:55:21 -0400

    Optimised image size, bug fixes

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy({ "CNAME": "CNAME" });
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   return {
     dir: {
